70ffb0abfab848e5fc5e0aa825d897372e3e3433
Igor Mogilnitskiy <igmo71@outlook.com>
2026-01-23 20:33:22 +0300

    mvp

--- Emploees/Domain/JobTitle.cs
+++ Emploees/Domain/JobTitle.cs
@@ -5,7 +5,7 @@ namespace Emploees.Domain
     public class JobTitle
     {
         [MaxLength(36)]
-        public required string Id { get; set; }
+        public string? Id { get; set; }
 
         [MaxLength(150)]
         public string? Name { get; set; }
