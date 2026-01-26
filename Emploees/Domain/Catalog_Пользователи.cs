a66213025977f310f4cf51f2ed61be2a22a8093d
Igor Mogilnitskiy <igmo71@outlook.com>
2026-01-26 16:39:08 +0300

    PersistentState

--- Emploees/Domain/Catalog_Пользователи.cs
+++ Emploees/Domain/Catalog_Пользователи.cs
@@ -5,7 +5,7 @@ namespace Emploees.Domain
 {
     public class Catalog_Пользователи : IHasRefKey, IHasDescription
     {
-        [MaxLength(AppSettings.GUID)] public required string Ref_Key { get; set; }
+        [MaxLength(AppSettings.GUID)] public string? Ref_Key { get; set; }
         [MaxLength(AppSettings.DESCRIPTION)] public string? Description { get; set; }
         public bool DeletionMark { get; set; }
     }
--- Emploees/Domain/Emploee.cs
+++ Emploees/Domain/Emploee.cs
@@ -1,6 +1,4 @@
-﻿using Emploees.Common;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Emploees.Domain
 {
